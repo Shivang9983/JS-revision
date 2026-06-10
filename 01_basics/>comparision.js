bb0947d28036c2e29588aeca2c48e0b967fa88f1
shivang kumar <133517596+Shivang9983@users.noreply.github.com>
2026-06-10 21:46:47 +0530

    Update >comparision.js

--- 01_basics/>comparision.js
+++ 01_basics/>comparision.js
@@ -12,10 +12,3 @@ console.log(null > 0);
 console.log(null == 0);
 console.log(null >= 0);
 
-console.log(undefined == 0);
-console.log(undefined > 0);
-console.log(undefined < 0);
-
-// === 
-
-console.log("2" === 2);
