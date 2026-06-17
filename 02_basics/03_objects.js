6003c61b5c01fa8abe39dd7c722e2a6a7a50a1bf
shivang kumar <133517596+Shivang9983@users.noreply.github.com>
2026-06-17 19:16:55 +0530

    learn object in simple update JS_user

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -7,12 +7,12 @@ const mySym = Symbol("key1")
 
 
 const JsUser = {
-    name: "Hitesh",
-    "full name": "Hitesh Choudhary",
+    name: "Shiavng",
+    "full name": "Shivang kumar",
     [mySym]: "mykey1",
     age: 18,
     location: "Jaipur",
-    email: "hitesh@google.com",
+    email: "shiavngkk@gmail.com",
     isLoggedIn: false,
     lastLoginDays: ["Monday", "Saturday"]
 }
@@ -22,9 +22,9 @@ const JsUser = {
 // console.log(JsUser["full name"])
 // console.log(JsUser[mySym])
 
-JsUser.email = "hitesh@chatgpt.com"
+JsUser.email = "shiavngkk@gmail.com"
 // Object.freeze(JsUser)
-JsUser.email = "hitesh@microsoft.com"
+JsUser.email = "shiavngkk@gmail.com"
 // console.log(JsUser);
 
 JsUser.greeting = function(){
