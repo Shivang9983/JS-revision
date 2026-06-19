4b278e63aa5f7eb06179cac10d4dbf3213108990
shivang kumar <133517596+Shivang9983@users.noreply.github.com>
2026-06-19 23:44:11 +0530

    Refactor functions and add calculateCartPrice
    Refactored addTwoNumbers function to remove duplicate return statement and added calculateCartPrice function.

--- 03_basics/>function.js
+++ 03_basics/>function.js
@@ -41,3 +41,10 @@ console.log(loginUserMessage("Shivang"))
 console.log(loginUserMessage("Kuma"))
 
 
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+console.log(calculateCartPrice(200, 400, 500, 2000))
+
+
