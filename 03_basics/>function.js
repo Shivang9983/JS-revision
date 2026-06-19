7e95a9a7001b072ec27dcc4cdcd65bb489b7d5aa
shivang kumar <133517596+Shivang9983@users.noreply.github.com>
2026-06-19 23:41:19 +0530

    addTwoNumber function example >function.js

---
+++ 03_basics/>function.js
@@ -0,0 +1,28 @@
+
+function sayMyName(){
+    console.log("S");
+    console.log("H");
+    console.log("I");
+    console.log("V");
+    console.log("A");
+    console.log("N");
+    console.log("G");
+}
+
+sayMyName()
+
+function addTwoNumbers(number1, number2){
+
+    console.log(number1 + number2);
+}
+
+function addTwoNumbers(number1, number2){
+
+    let result = number1 + number2
+    return result
+    return number1 + number2
+}
+
+const result = addTwoNumbers(3, 5)
+
+console.log("Result: ", result);
