0b0929d86e7385f4c986acab3e8cdfd93b211a39
shivang kumar <133517596+Shivang9983@users.noreply.github.com>
2026-07-02 15:28:22 +0530

    Refactor pattern printing in Pattern_Ex.js With  nested loop

--- Pattern_prog/Pattern_Ex.js
+++ Pattern_prog/Pattern_Ex.js
@@ -23,4 +23,14 @@ console.log(" * * * * *");
 * * * * 
 * * * * * 
 
-  
+  let Prompt = require('prompt-sync')();
+let a = Number(Prompt("enter your Number: "));
+
+for (let i = 1; i<=a; i++){
+    for (let j = 1; j<=a; j++){
+        process.stdout.write("* ");
+    }
+    console.log();
+}
+
+
