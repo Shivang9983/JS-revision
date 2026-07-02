539355b98f656dc70abe6145daf9da1218634523
shivang kumar <133517596+Shivang9983@users.noreply.github.com>
2026-07-02 15:22:43 +0530

    Refactor userinput.js to include user prompt

--- Pattern_prog/userinput.js
+++ Pattern_prog/userinput.js
@@ -9,3 +9,9 @@ process.stdout.write("hello ");
 process.stdout.write("world");
 
 // OUTPUT = hello world
+
+
+let Prompt = require('prompt-sync')();
+let a = Prompt("enter your name: ");
+
+process.stdout.write("hello " + a);
