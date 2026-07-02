b5ad3a8de70e043691c3f9d0ecaad3edae31772b
shivang kumar <133517596+Shivang9983@users.noreply.github.com>
2026-07-02 15:21:46 +0530

    Change output to uppercase and add comments

--- Pattern_prog/userinput.js
+++ Pattern_prog/userinput.js
@@ -1,4 +1,11 @@
+console.log("HELLO");
+console.log("WORLD");
+
+// OUTPUT = HELLO
+//          WORLD
+
+
 process.stdout.write("hello ");
 process.stdout.write("world");
 
-
+// OUTPUT = hello world
