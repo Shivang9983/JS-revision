249e1c2c74d976bc69f6578ddb8a97893e0be934
shivang kumar <133517596+Shivang9983@users.noreply.github.com>
2026-07-02 15:30:00 +0530

    Using nested loop to print simple pattern

--- Pattern_prog/Pattern_Ex.js
+++ Pattern_prog/Pattern_Ex.js
@@ -23,11 +23,26 @@ console.log(" * * * * *");
 * * * * 
 * * * * * 
 
+    
+// pattern == 1
+    
   let Prompt = require('prompt-sync')();
 let a = Number(Prompt("enter your Number: "));
 
 for (let i = 1; i<=a; i++){
     for (let j = 1; j<=a; j++){
+        process.stdout.write("* ");
+    }
+    console.log();
+}
+
+// pattern == 2
+
+let Prompt = require('prompt-sync')();
+let a = Number(Prompt("enter your Number: "));
+
+for (let i = 1; i<=a; i++){
+    for (let j = 1; j<=i; j++){
         process.stdout.write("* ");
     }
     console.log();
